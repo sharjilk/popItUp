87f0d51d7878c84188037e010fb448bccd404db0
sharjilk <sharjilk62@gmail.com>
2016-01-08 16:10:23 +0530

    Update jquery.popitup.js

--- jquery.popitup.js
+++ jquery.popitup.js
@@ -1,3 +1,12 @@
+/**
+ * @name: PopItUp
+ * @type: Jquery Plugin
+ * @author: Sharjil Khan
+ * @demo: "working on it"
+ * @version: 0.0.1
+ * @requires jQuery library
+ */
+ 
 (function($){
 
   // Plugin function
@@ -5,25 +14,34 @@
 
     /* defaults */
     var defaults = {
-      widthSet     :  '300',
-      overlayColor :  "#000",
-      autoClose    :  false,
-      animation    :  null,
-      colorChange  :  {
-                        color      : null,
-                        background : null
-                      },
-      chase        :  false,
-      chaseSpeed   :  "500",
-      onCloseModal :  function(){},
-      onOpenModal  :  function(){}
+      widthSet       :  "",                      /** only integer */
+      overlayColor   :  "#000",                  /** only hexcode */
+      overlayOpacity :  "0.5",                   /** values between 0.1 to 1 */
+      autoClose      :  false,                   /** boolean */
+      animation      :  null,                    /** only slideDown, slideUp, slideLeft, slideRight */ 
+      colorChange    :  {                        /** To change the background and text color of popup */
+                          color      : null,
+                          background : null
+                        },
+      chase          :  true,                   /** booleans */
+      chaseSpeed     :  "500",                   /** only integer */
+      fixedModal     :  false,                   /** booleans */
+      modalPosition  :  [],                      /** [integer, integer] */
+      containerToLoad:  "",                      /** string */
+      urlToLoad      :  "",                      /** string */
+      imageToLoad    :  "",                      /** string */
+
+      /** callback methods */
+      onCloseModal   :  function(){},            
+      onOpenModal    :  function(){}
     }
 
     var opts = $.extend(true, {}, defaults, options);
     
     /* validate options */
     var regExpWidth = /^[0-9]/;
-    var regExpColor = /^[#][A-Fa-f0-9]/ /* only hexcode is required */
+    var regExpOpacity = /^[0-1][.][0-9]$/;
+    var regExpColor = /^[#][A-Fa-f0-9]/; /* only hexcode is required */
 
     $.each(opts, function validateOptions(key, val){
 
@@ -41,6 +59,13 @@
         }
       }
 
+      /* overlay opacity */
+      if( key == 'overlayOpacity' ){
+        if( val.match(regExpOpacity) === null ){
+          opts[key] = defaults[key];
+        }
+      }
+
       /* autoClose */
       if( key === 'autoClose' ){
         if( val !== true ){
@@ -49,19 +74,36 @@
       }
 
       /* colorChange */
-      if( key === 'colorChange' ){
-        if( val.color.match(regExpColor ) === null ){
+
+      /** IT CHECKS THE NULL VALUE... NEED TO BE FIXED */
+
+      /*if( key === 'colorChange' ){
+        if( val.color.match(regExpColor ) === "" ){
           opts[key]['color'] = defaults[key]['color'];
         }
-        if( val.background.match(regExpColor ) === null ){
+        if( val.background.match(regExpColor ) === "" ){
           opts[key]['background'] = defaults[key]['background'];
         }
-      }
+      }*/
 
       /* autoClose */
       if( key === 'chase' ){
+        if( val !== false ){
+          opts[key] = defaults[key];
+        }
+      }
+
+      /* fixedModal */
+      if( key === 'fixedModal' ){
         if( val !== true ){
           opts[key] = defaults[key];
+        }
+      }
+
+      /* modalPosition */
+      if( key === 'modalPosition' ){
+        if( opts.modalPosition.length > 0 ){
+          opts['chase'] = false;
         }
       }
 
@@ -70,15 +112,49 @@
     /* open popup */
     $modal = this;
     $('body').append('<div class="popitup-overlay"></div>');
-    $('.popitup-overlay').css("background",opts.overlayColor);
+    $('.popitup-overlay').css({
+      "background" : opts.overlayColor,
+      "opacity"    : opts.overlayOpacity
+    });
 
+    if(opts.fixedModal){
+      var $style = "fixed";
+    }else{
+      $style = "absolute";
+    }
     $modal.css({
       "width"    :  opts.widthSet+'px',
-      "position" :  "absolute",
+      "position" :  $style,
       "display"  :  "block",
       "opacity"  :  "0",
       "z-index"  :  "99999",
     });
+
+    /* loading ajax request */
+    if( (opts.containerToLoad != "") ){
+
+      if( (opts.urlToLoad != "") ){
+        var request;
+        if(window.XMLHttpRequest){
+          request = new XMLHttpRequest();
+        }
+        else{
+          request = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        request.open('GET', opts.urlToLoad);
+          if( request.readyState > 0 ){
+            $(opts.containerToLoad).load(opts.urlToLoad);
+          }
+        request.send();
+      }
+
+      if( (opts.imageToLoad != "") ){
+        $(opts.containerToLoad).html("<img src='' class='popitup-img' />");
+        $(".popitup-img").attr('src', opts.imageToLoad);
+      }
+
+    }
+
     $margin_left = $modal.outerWidth()/2;
     $margin_top  = $modal.outerHeight()/2;
     $modal.css({
@@ -86,24 +162,51 @@
       "margin-left" : "-" + $margin_left + "px"
     });
 
-    switch (opts.animation)
-    {
-      case 'slideDown': 
-        $modal.css("top","0")
-        .animate({top: ( ($(window).height() + $(window).scrollTop()) / 2 ) + "px", opacity: "1"}, 300);
-        applyModalOffset();
-        break;
+    /* check for modal position */
+    if( opts.modalPosition.length > 0 ){
+      $modal.css({
+        'top'      :   $(window).scrollTop() + opts.modalPosition[0], 
+        'left'     :   opts.modalPosition[1],
+        'margin'   :   0, 
+        'opacity'  :  '1',
+        'position' :  'absolute'
+      });
 
-      case 'slideUp':
-        $modal.css("bottom","0")
-        .animate({top: ( ($(window).height() + $(window).scrollTop()) / 2 ) + "px", opacity: "1"}, 300);
-        applyModalOffset();
-        break;
+    }
+    else{
 
-      default: 
-        $modal.css({'top':'50%', 'opacity':'1'});
-        applyModalOffset();
-        break;
+      switch (opts.animation)
+      {
+        case 'slideDown': 
+          $modal.css("top","0")
+          .animate({top: $(window).height() / 2 + $(window).scrollTop()+"px", opacity: "1"}, 300);
+          applyModalOffset();
+          break;
+
+        case 'slideUp':
+          $modal.css("bottom","0")
+          .animate({top: $(window).height() / 2 + $(window).scrollTop()+"px", opacity: "1"}, 300);
+          applyModalOffset();
+          break;
+
+        case 'slideLeft':
+          $modal.css({"left":"0",top: $(window).height() / 2 + $(window).scrollTop()+"px"})
+          .animate({ left: "50%", opacity: "1"}, 300);
+          applyModalOffset();
+          break;
+
+        case 'slideRight':
+          $modal.css({"right":"0",top: ( ($(window).height() + $(window).scrollTop()) / 2 ) + "px"})
+          .animate({ left: "50%", opacity: "1"}, 300);
+          applyModalOffset();
+          break;
+
+        default: 
+          $modal.css({'top': $(window).height() / 2 + $(window).scrollTop()+"px" , 'opacity':'1'});
+          applyModalOffset();
+          break;
+      }
+
     }
 
     opts.onOpenModal();
@@ -120,57 +223,89 @@
     if(opts.autoClose){
 
       $('.popitup-overlay').bind('click', function(){
-        switch (opts.animation)
-        {
-          case 'slideDown': 
-            $modal.animate({top: "0", opacity: "0", display: "none"}, 300);
-            applyModalOffset();
-            setTimeout(function(){
-              $modal.css({'top':'','left':'','right':'','bottom':''});
-            }, 300);
-            break;
 
-          case 'slideUp':
-            $modal.animate({top: "100%", opacity: "0", display: "none"}, 300);
-            applyModalOffset();
-            setTimeout(function(){
-              $modal.css({'top':'','left':'','right':'','bottom':''});
-            }, 300);
-            break;
+        if( opts.modalPosition.length > 0 ){
 
-          default: 
-            $modal.css({'top':'','left':'','right':'','bottom':''})
-            .hide();
-            break;
+          $modal.css({'top':'','left':'','right':'','bottom':''})
+          .hide();
+          $('.popitup-overlay').remove();
+
+        }
+        else{
+
+          switch (opts.animation)
+          {
+            case 'slideDown': 
+              $modal.animate({top: "0", opacity: "0", display: "none"}, 300);
+              applyModalOffset();
+              setTimeout(function(){
+                $modal.removeAttr("style");
+              }, 300);
+              break;
+
+            case 'slideUp':
+              $modal.animate({top: "100%", opacity: "0", display: "none"}, 300);
+              applyModalOffset();
+              setTimeout(function(){
+                $modal.removeAttr("style");
+              }, 300);
+              break;
+
+            case 'slideLeft':
+              $modal.animate({left: "0", opacity: "0", display: "none"}, 300);
+              applyModalOffset();
+              setTimeout(function(){
+                $modal.removeAttr("style");
+              }, 300);
+              break;
+
+            case 'slideRight':
+              $modal.animate({left: "100%", opacity: "0", display: "none"}, 300);
+              applyModalOffset();
+              setTimeout(function(){
+                $modal.removeAttr("style");
+              }, 300);
+              break;
+
+            default: 
+              $modal.removeAttr("style")
+              .hide();
+              $('.popitup-overlay').remove();
+              break;
+          }
+
         }
 
         setTimeout(function(){
           $('.popitup-overlay').remove();
         }, 300);
-        opts.onCloseModal.call();
+        opts.onCloseModal();
 
       });
 
     }
     
     /* popup follow while scroll */
-    if(opts.chase){
+    //var posTemp = opts.modalPosition.length > 0;
+    if(!opts.fixedModal ){
+      if(opts.chase === true){
 
-      var lastScrollTop = 0;
-      $(window).scroll(function(event){
+        var lastScrollTop = 0;
+        $(window).scroll(function(event){
 
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop){
-          /* to bottom */
-           $modal.stop().animate({"marginTop": ($(window).scrollTop() + (-93)) + "px"}, +opts.chaseSpeed);
-        } else {
-          /* to top */
-          $modal.stop().animate({"marginTop": ($(window).scrollTop() + (-93)) + "px"}, +opts.chaseSpeed);
-        }
-        lastScrollTop = st;
+          var st = $(this).scrollTop();
+          if (st > lastScrollTop){
+            /* to bottom */
+             $modal.stop().animate({"top": $(window).height() / 2 + $(window).scrollTop()+"px"}, +opts.chaseSpeed);
+          } else {
+            /* to top */
+            $modal.stop().animate({"top": $(window).height() / 2 + $(window).scrollTop()+"px"}, +opts.chaseSpeed);
+          }
+          lastScrollTop = st;
 
-      });
+        });
 
+      }
     }
 
     /* Finding close button */
@@ -180,28 +315,56 @@
       if($varClose){
         $(this).bind('click', function(){
 
-          switch (opts.animation)
-          {
-            case 'slideDown': 
-              $modal.animate({top: "0", opacity: "0", display: "none"}, 300);
-              applyModalOffset();
-              setTimeout(function(){
-                $modal.css({'top':'','left':'','right':'','bottom':''});
-              }, 300);
-              break;
+          if( opts.modalPosition.length > 0 ){
 
-            case 'slideUp':
-              $modal.animate({top: "100%", opacity: "0", display: "none"}, 300);
-              applyModalOffset();
-              setTimeout(function(){
-                $modal.css({'top':'','left':'','right':'','bottom':''});
-              }, 300);
-              break;
+            $modal.css({'top':'','left':'','right':'','bottom':''})
+            .hide();
+            $('.popitup-overlay').remove();
 
-            default: 
-              $modal.css({'top':'','left':'','right':'','bottom':''})
-              .hide();
-              break;
+          }
+          else{
+
+            switch (opts.animation)
+            {
+              case 'slideDown': 
+                $modal.animate({top: "0", opacity: "0", display: "none"}, 300);
+                applyModalOffset();
+                setTimeout(function(){
+                  $modal.removeAttr("style");
+                }, 300);
+                break;
+
+              case 'slideUp':
+                $modal.animate({top: "100%", opacity: "0", display: "none"}, 300);
+                applyModalOffset();
+                setTimeout(function(){
+                  $modal.removeAttr("style");
+                }, 300);
+                break;
+
+              case 'slideLeft':
+                $modal.animate({left: "0", opacity: "0", display: "none"}, 300);
+                applyModalOffset();
+                setTimeout(function(){
+                  $modal.removeAttr("style");
+                }, 300);
+                break;
+
+              case 'slideRight':
+                $modal.animate({left: "100%", opacity: "0", display: "none"}, 300);
+                applyModalOffset();
+                setTimeout(function(){
+                  $modal.removeAttr("style");
+                }, 300);
+                break;
+
+              default: 
+                $modal.removeAttr("style")
+                .hide();
+                $('.popitup-overlay').remove();
+                break;
+            }
+
           }
 
           setTimeout(function(){
